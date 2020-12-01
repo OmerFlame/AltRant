5b237c7ee33274b7efa1ece78f7b4118eb55bafc
Omer Shamai <renginald.bagel@gmail.com>
2020-12-01 09:26:33 +0200

    Initial Commit

---
+++ AltRant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AltRant
+//
+//  Created by Omer Shamai on 12/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
